5d8869318f95f4ff78757efe70526a59a9ddf099
fatmanurshain <shnfatmanurr@gmail.com>
2024-07-07 20:00:48 +0300

    first commit

---
+++ Flickr Client App/App/Core/Networking/NetworkManager.swift
@@ -0,0 +1,8 @@
+//
+//  NetworkManager.swift
+//  Flickr Client App
+//
+//  Created by Fatmanur Şahin on 5.07.2024.
+//
+
+import Foundation
---
+++ Flickr Client App/Screens/Recent Photos/Model/Photo.swift
@@ -0,0 +1,8 @@
+//
+//  Photo.swift
+//  Flickr Client App
+//
+//  Created by Fatmanur Şahin on 1.07.2024.
+//
+
+import Foundation
